499f0f83f4df489fb312e1645c0d0a17049278e7
Julia_Chu <z3211232001@gmail.com>
2024-11-27 11:30:08 -0800

    finished

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 $(function() {
   // your code here
+  //Exercise 1
+  $('.accordion-header').on('click', function() {
+    $('.accordion-content').slideUp() //everything slideup
+    if(!$(this).next('.accordion-content').is(':visible')) {
+      $(this).next('.accordion-content').slideDown()
+    }
+  })
+
+  //Exercise 2
+  $('.todos').on('click', () => {
+    $.ajax({
+      url: 'https://dummyjson.com/todos',
+      type: 'GET',
+      success: function(response) {
+        const todos = response.todos
+        $('ul').empty() //clean the list to avoid repeat
+        todos.forEach(todo => {
+          $('ul').append(`
+            <li> ${todo.todo} </li>
+            `)
+        })
+      },
+      error: function(err) {
+        console.log(err)
+      }
+    })  
+  })
 })
